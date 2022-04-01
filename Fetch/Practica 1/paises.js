5fb318305b2798cc307263593284ffe211257566
AlanGF7 <af189107@gmail.com>
2022-03-31 18:08:12 -0600

    Personalización
    Solo diseño

--- Fetch/Practica 1/paises.js
+++ Fetch/Practica 1/paises.js
@@ -1,12 +1,33 @@
-var paises = document.getElementById("paises")
+var mexico = document.getElementById("Mexico")
+var panama = document.getElementById("Panama")
+var colombia = document.getElementById("Colombia")
 
+var textMexico = document.getElementById("textMexico")
+var textPanama = document.getElementById("textPanama")
+var textColombia = document.getElementById("textColombia")
+
+
+var contador = 0;
 fetch("datos.json").then(function(response){
-    console.log(response)
     response.json().then(function(datos){
         datos.forEach(registro => {
-            let nombre = document.createElement("p");
-            nombre.textContent = "País: " + registro.country + ", casos: " + registro.cases
-            paises.appendChild(nombre);
+            let pais = document.createElement("h2");
+            pais.textContent = "País: " + registro.country + ", Casos: " + registro.cases
+            if(contador == 0) 
+            {
+                textMexico.textContent = "País: " + registro.country + ", Casos: " + registro.cases
+                mexico.appendChild(textMexico);
+            }
+            if(contador == 1) 
+            {
+                textPanama.textContent = "País: " + registro.country + ", Casos: " + registro.cases
+                panama.appendChild(textPanama);
+            }
+            if(contador == 2) {
+                textColombia.textContent = "País: " + registro.country + ", Casos: " + registro.cases
+                colombia.appendChild(textColombia);
+            }
+            contador++;
 
         });
     });
