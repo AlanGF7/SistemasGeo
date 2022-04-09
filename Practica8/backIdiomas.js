0b272529d62fd54e5341ad5d4a5a265c12e1a13d
AlanGF7 <af189107@gmail.com>
2022-04-09 18:33:00 -0500

    WatchPositions

---
+++ Practica8/backIdiomas.js
@@ -0,0 +1,29 @@
+
+var coordenadas = {
+    lat: 29.099358,
+    lng: -110.956896,
+  };
+  
+  var propiedades = {
+    center: coordenadas,
+    zoom: 3,
+  };
+
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const idioma = urlParams.get("idioma");
+  console.log(urlParams);
+
+  var script = document.createElement("script");
+  script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyCMrQgga-C5zCuZLTVk2MPVzX7naqKZXZU&callback=iniciarMapa&language=${idioma}`;
+
+  
+  document.head.appendChild(script);
+
+  var map;
+  
+  function iniciarMapa() {
+    
+    map = new google.maps.Map(document.getElementById("mapa"), propiedades);
+  
+}
---
+++ Practica9/backEstilos.js
@@ -0,0 +1,209 @@
+function iniciarMapa() {
+
+
+
+  var coordenadas = {
+    lat: 21.152639,
+    lng: -101.711598,
+  };
+
+  var propiedades = {
+    center: coordenadas,
+    zoom: 3,
+    mapTypeControlOptions: {
+      mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain', 'style_map']
+    }
+  };
+
+  var styledMap = new google.maps.StyledMapType([
+      {
+        elementType: 'geometry',
+        stylers: [
+          {
+            "color": "#212121"
+          }
+        ]
+      },
+      {
+        elementType: 'labels.icon',
+        stylers: [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        elementType: 'labels.text.stroke',
+        stylers: [
+          {
+            "color": "#212121"
+          }
+        ]
+      },
+      {
+        featureType: 'administrative',
+        elementType: 'geometry',
+        stylers: [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        featureType: 'administrative.country',
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            "color": "#c33737"
+          }
+        ]
+      },
+      {
+        featureType: 'administrative.land_parcel',
+        stylers: [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            "color": "#bdbdbd"
+          }
+        ]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [
+          {
+            "color": "#181818"
+          }
+        ]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            "color": "#616161"
+          }
+        ]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.stroke',
+        stylers: [
+          {
+            "color": "#1b1b1b"
+          }
+        ]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry.fill',
+        stylers: [
+          {
+            "color": "#84c238"
+          }
+        ]
+      },
+      {
+        featureType: 'road',
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            "color": "#8a8a8a"
+          }
+        ]
+      },
+      {
+        featureType: 'road.arterial',
+        elementType: 'geometry',
+        stylers: [
+          {
+            "color": "#cf4444"
+          }
+        ]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [
+          {
+            "color": "#78c4ce"
+          }
+        ]
+      },
+      {
+        featureType: 'road.highway.controlled_access',
+        elementType: 'geometry',
+        stylers: [
+          {
+            "color": "#4e4e4e"
+          }
+        ]
+      },
+      {
+        featureType: 'road.local',
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            "color": "#c8e774"
+          }
+        ]
+      },
+      {
+        featureType: 'transit',
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [
+          {
+            "color": "#000000"
+          }
+        ]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [
+          {
+            "color": "#3d3d3d"
+          }
+        ]
+      }
+  ], { name: "Mapa oscuro" });
+
+  var map = new google.maps.Map(document.getElementById("mapa"), propiedades);
+
+  map.mapTypes.set("style_map", styledMap);
+  map.setMapTypeId('style_map');
+}
